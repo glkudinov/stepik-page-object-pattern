c3c838d381f5d662bbddfb339a0bf72c75b74ff5
Gleb <monachusgl@gmail.com>
2022-06-14 23:48:15 +0300

    Добавлена параметризация @pytest.mark.parametrize для поиска бага, после введения новых промо-акций

--- test_product_page.py
+++ test_product_page.py
@@ -3,9 +3,18 @@ import pytest
 from pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    #url = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('url', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                 pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+                                              "/?promo=offer7", marks=pytest.mark.xfail),
+                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, url):
     page = ProductPage(browser, url)
     page.open()
     page.add_to_cart()
@@ -14,5 +23,3 @@ def test_guest_can_add_product_to_basket(browser):
     page.should_be_success_message_price_in_cart()
     page.should_be_correct_product_name()
     page.should_be_correct_adding_product_price()
-
-
