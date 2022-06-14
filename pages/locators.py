c3b938726da5966a090fe8d714e52a779a7861b1
Gleb <monachusgl@gmail.com>
2022-06-14 17:36:43 +0300

    Организованы переходы между страницами

--- pages/locators.py
+++ pages/locators.py
@@ -1,5 +1,14 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators:
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTER_EMAIL_ADDRESS = (By.XPATH, "//input[@name='registration-email']")
+    REGISTER_PASSWORD = (By.XPATH, "//input[@name='registration-password1']")
+    REGISTER_PASSWORD_CONFIRM = (By.XPATH, "//input[@name='registration-password2']")
+    REGISTER_BUTTON = (By.XPATH, "//button[@name='registration_submit']")
